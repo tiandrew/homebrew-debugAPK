9da0b5107b0e315b3aeef58cfacf7a1413cb3ed8
Andrew Tian <tiandrew@umich.edu>
2020-03-12 15:11:39 -0700

    update version

--- debugapk.rb
+++ debugapk.rb
@@ -3,8 +3,8 @@
 class Debugapk < Formula
   desc "This script is used to make an APK debuggable and usable with Charles"
   homepage "https://github.com/tiandrew/DebugAPK"
-  url "https://github.com/tiandrew/DebugAPK/archive/v1.0.5.tar.gz"
-  version "1.0.5"
+  url "https://github.com/tiandrew/DebugAPK/archive/v1.0.6.tar.gz"
+  version "1.0.6"
   sha256 "92d0cdc0f64217b7f9ed6227f3cd56d6206a83af2e0478b2c668b848928a06ae"
 
   def install
