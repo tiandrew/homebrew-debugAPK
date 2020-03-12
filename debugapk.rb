1e8ae5712d04c4d34575bf41d491a4c64ee1c21d
Andrew Tian <tiandrew@umich.edu>
2020-03-12 14:28:51 -0700

    Update sha256 key

--- debugapk.rb
+++ debugapk.rb
@@ -5,7 +5,7 @@ class Debugapk < Formula
   homepage "https://github.com/tiandrew/DebugAPK"
   url "https://github.com/tiandrew/DebugAPK/archive/v1.0.3.tar.gz"
   version "1.0.3"
-  sha256 "40824f825a863fc19d096836e8574e6652f9b38de3587add48a68a5707b8c955"
+  sha256 "6f4915eed75d367b068ab33884de54f4a3a19b3176ddaa828ffdf1ca12bdfd0d"
 
   def install
       bin.install "debugapk"
